88cc4a152d25615e83ad4307c0245273cd83903d
wifon <kaziorigor0902@gmail.com>
2024-01-05 19:11:43 +0100

    adding city search via input

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 import { config } from "./config.js";
 
 const API_KEY = config.API_KEY;
-const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=Paris&units=metric&appid=${API_KEY}`;
+const button = document.querySelector("#check-button");
 
-fetch(API_URL)
-  .then((res) => res.json())
-  .then((weatherRaw) => {
-    let city = {
-      name: weatherRaw.name,
-      country: weatherRaw.sys.country,
-      temp: weatherRaw.main.temp,
-      humidity: weatherRaw.main.humidity,
-      sunrise: weatherRaw.sys.sunrise,
-      sunshine: weatherRaw.sys.sunset,
-      weather: weatherRaw.weather.main,
-      weatherDesc: weatherRaw.weather.description,
-    };
-    console.log(weatherRaw);
-  });
+button.addEventListener("click", () => {
+  let inputCityName = document.querySelector("#city-name").value;
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${inputCityName}&units=metric&appid=${API_KEY}`;
+  dataFetching(API_URL);
+});
+
+function dataFetching(API_URL) {
+  fetch(API_URL)
+    .then((res) => res.json())
+    .then((weatherRaw) => {
+      let city = {
+        name: weatherRaw.name,
+        country: weatherRaw.sys.country,
+        temp: weatherRaw.main.temp,
+        humidity: weatherRaw.main.humidity,
+        sunrise: weatherRaw.sys.sunrise,
+        sunshine: weatherRaw.sys.sunset,
+        weather: weatherRaw.weather.main,
+        weatherDesc: weatherRaw.weather.description,
+      };
+      console.log(weatherRaw);
+    });
+}
