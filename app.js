bc6640344cf1d644af659c32ef719b02e45b528d
wifon <kaziorigor0902@gmail.com>
2024-01-06 16:08:35 +0100

    feat: adding data export and function that change the region code to the full name

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import { config } from "./config.js";
+import { regionNames } from "./region.js";
 
 const API_KEY = config.API_KEY;
 const button = document.querySelector("#check-button");
+
+const cityName = document.querySelector("#city");
+const country = document.querySelector("#country");
+const temp = document.querySelector("#temp");
+const humidity = document.querySelector("#humidity");
+const weather = document.querySelector("#weather");
+const weatherDesc = document.querySelector("#weather-desc");
 
 button.addEventListener("click", () => {
   let inputCityName = document.querySelector("#city-name").value;
@@ -23,6 +31,18 @@ function dataFetching(API_URL) {
         weather: weatherRaw.weather[0].main,
         weatherDesc: weatherRaw.weather[0].description,
       };
-      console.log(city);
+      exportData(city);
+      console.log(weatherRaw);
     });
+}
+
+function exportData(city) {
+  cityName.innerText = city.name;
+  country.innerText = regionNames.of(city.country);
+  temp.innerText = city.temp;
+  humidity.innerText = city.humidity;
+  weather.innerText = city.weather;
+  weatherDesc.innerText = city.weatherDesc;
+
+  console.log(city);
 }
---
+++ region.js
@@ -0,0 +1,1 @@
+export const regionNames = new Intl.DisplayNames(["en"], { type: "region" });
